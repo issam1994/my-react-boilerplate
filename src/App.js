ffededb239d36918f18e51bd69cfe9603b9d0f86
issam1994 <issamaitouahmane@gmail.com>
2020-03-07 23:14:04 +0100

    added & configured postcss, tailwindcss , autoprefixer & purgecss. deleted some files that come with the default create-react-app created some folders to prepare for more configuration & to define the structure of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-teal-300">
+      <div className="max-w-xl flex flex-wrap items-center justify-center p-4 shadow-lg bg-white rounded-lg m-2">
+        <img src={logo} className="w-24 h-24 rounded-full shadow-lg m-4" alt="logo" />
+        <div className="text-center sm:text-left">
+          <div className="text-xl font-semibold">React.js App Boilerplate</div>
+          <div className="text-base text-gray-500">By Issam Ait Ouahmane</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//tailwindcss
+import './styles/tailwindcss.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

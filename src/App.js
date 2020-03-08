bd21c03e75361d37f290f9d1695581acc97fe02e
issam1994 <issamaitouahmane@gmail.com>
2020-03-08 22:16:13 +0100

    changed the output of postcss processing to index.css that I newly created instead of using the same file that was used as source

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-teal-300">
       <div className="max-w-xl flex flex-wrap items-center justify-center p-4 shadow-lg bg-white rounded-lg m-2">
-        <img src={logo} className="w-24 h-24 rounded-full shadow-lg m-4" alt="logo" />
+        <img src={logo} className="w-24 h-24 bg-gray-700 rounded-full shadow-lg mr-4" alt="logo" />
         <div className="text-center sm:text-left">
-          <div className="text-xl font-semibold">React.js App Boilerplate</div>
-          <div className="text-base text-gray-500">By Issam Ait Ouahmane</div>
+          <div className="text-xl font-semibold">React App Boilerplate</div>
+          <div className="text-sm font-semibold text-gray-500">By Issam Ait Ouahmane</div>
         </div>
       </div>
     </div>
